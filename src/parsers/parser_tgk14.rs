902c590af6ab8f0c17a19ade79e18b594a841830
rummolprod999 <rummolprod999@gmail.com>
2019-04-30 09:46:38 +0300

    fix issue

--- src/parsers/parser_tgk14.rs
+++ src/parsers/parser_tgk14.rs
@@ -39,7 +39,7 @@ impl<'a> ParserTgk14<'a> {
         self.connect_string = c_s;
         let url_b = "https://www.tgk-14.com/trade/vesti.sections.php?&num_page=";
 
-        for d in (3..=1).rev() {
+        for d in (1..=3).rev() {
             let url = format!("{}{}", url_b, d);
             let page = httptools::HttpTools::get_page_text(&url);
             match page {
